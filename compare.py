a2bd10d8ca0d2bdd5c43cdeab17a575267adb973
tas9666 <93866942+tas9666@users.noreply.github.com>
2021-12-25 08:47:56 +0400

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,8 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+print(" Program to demonstrate the use of Pyton library-")
+num=121.121
+print(math.sqrt(num))
+print(math.ceil(num))
+print(math.floor(num))
